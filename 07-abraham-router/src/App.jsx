d161e5e559ed6f3b789a1f8968239e0e1dee6415
Abraham Galue <abrahamgalue512@gmail.com>
2023-10-19 14:04:49 -0400

    feat: use lazy loading

--- 07-abraham-router/src/App.jsx
+++ 07-abraham-router/src/App.jsx
@@ -1,19 +1,21 @@
-import './App.css'
-import HomePage from './pages/Home'
-import AboutPage from './pages/About'
+// importa de manera dinamica los componentes
+// Y hasta que no los necesitamos no los utiliza
+import { lazy, Suspense } from 'react'
+// import HomePage from './pages/Home'
+// import AboutPage from './pages/About' // import estático
 import Page404 from './pages/404'
 import SearchPage from './pages/Search'
 
 import { Router } from './Router'
+import { Route } from './Route'
+
+const LazyHomePage = lazy(() => import('./pages/Home.jsx')) // lazy loading
+const LazyAboutPage = lazy(() => import('./pages/About.jsx')) // import dinamico
 
 const appRoutes = [
   {
-    path: '/',
-    Component: HomePage,
-  },
-  {
-    path: '/about',
-    Component: AboutPage,
+    path: '/:lang/about', // /es/about
+    Component: LazyAboutPage,
   },
   {
     path: '/search/:query', // /search/loquesea
@@ -24,7 +26,12 @@ const appRoutes = [
 function App() {
   return (
     <main>
-      <Router routes={appRoutes} defaultComponent={Page404} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Router routes={appRoutes} defaultComponent={Page404}>
+          <Route path='/' Component={LazyHomePage} />
+          <Route path='/about' Component={LazyAboutPage} />
+        </Router>
+      </Suspense>
     </main>
   )
 }
--- 07-abraham-router/src/Router.jsx
+++ 07-abraham-router/src/Router.jsx
@@ -1,8 +1,9 @@
 import { EVENTS } from './consts'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, Children } from 'react'
 import { match } from 'path-to-regexp'
 
 export function Router({
+  children,
   routes = [],
   defaultComponent: DefaultComponent = () => <h1>404</h1>,
 }) {
@@ -24,7 +25,17 @@ export function Router({
 
   let routeParams = {}
 
-  const Page = routes.find(({ path }) => {
+  // add routes from children <Route /> components
+  const routesFromChildren = Children.map(children, ({ props, type }) => {
+    const { name } = type
+    const isRoute = name === 'Route'
+
+    return isRoute ? props : null
+  })
+
+  const routesToUse = routes.concat(routesFromChildren).filter(Boolean)
+
+  const Page = routesToUse.find(({ path }) => {
     if (path === currentPath) return true
 
     // hemos usado path-to-regexp
