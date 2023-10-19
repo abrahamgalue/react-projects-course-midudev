4d7af77b469a20fb99f357fbfc7194c4f8784f88
Abraham Galue <abrahamgalue512@gmail.com>
2023-10-19 14:05:15 -0400

    feat: add language support with i18n

--- 07-abraham-router/src/pages/About.jsx
+++ 07-abraham-router/src/pages/About.jsx
@@ -1,19 +1,41 @@
 import { Link } from '../Link.jsx'
 
-export default function AboutPage() {
+console.log('Estamos importando el archivo About.jsx')
+
+const i18n = {
+  es: {
+    title: 'Sobre nosotros',
+    description:
+      'Hola! Me llamo Abraham Galue y estoy creando un clon de React Router.',
+    button: 'Ir a Home Page',
+  },
+  en: {
+    title: 'About us',
+    description:
+      'Hello! My name is Abraham Galue and I am creating a React Router clone.',
+    button: 'Go to Home Page',
+  },
+}
+
+const useI18n = lang => {
+  return i18n[lang] || i18n.en
+}
+
+export default function AboutPage({ routeParams }) {
+  const i18n = useI18n(routeParams.lang ?? 'es')
+
   return (
     <>
-      <h1>About</h1>
+      <h1>{i18n.title}</h1>
       <div>
         <img
           src='https://www.mascotarios.org/wp-content/uploads/2011/08/DogodeBurdeos.jpg'
           alt='Un bonito canino'
+          width={260}
         />
       </div>
-      <p>
-        Hola! Me llamo Abraham Galue y estoy creando un clon de React Router.
-      </p>
-      <Link to='/'>Ir a Home Page</Link>
+      <p>{i18n.description}</p>
+      <Link to='/'>{i18n.button}</Link>
     </>
   )
 }
