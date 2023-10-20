91ad8258b5c6c11ba4de97b5735290ac0b85e03a
Abraham Galue <abrahamgalue512@gmail.com>
2023-10-20 13:33:04 -0400

    test: adding render tests

--- 07-abraham-router/src/Router.test.jsx
+++ 07-abraham-router/src/Router.test.jsx
@@ -1,11 +1,18 @@
-import { describe, it, expect } from 'vitest'
-import { render, screen, cleanup } from '@testing-library/react'
-import { Router } from './Router'
-import { beforeEach } from 'vitest'
+import { describe, it, expect, beforeEach, vi } from 'vitest'
+import { render, screen, cleanup, fireEvent } from '@testing-library/react'
+import { Router } from './Router.jsx'
+import { Route } from './Route.jsx'
+import { Link } from './Link.jsx'
+import { getCurrentPath } from './utils/getCurrentPath.js'
+
+vi.mock('./utils/getCurrentPath.js', () => ({
+  getCurrentPath: vi.fn(),
+}))
 
 describe('Router', () => {
   beforeEach(() => {
     cleanup()
+    vi.clearAllMocks()
   })
 
   it('should render without problems', () => {
@@ -16,5 +23,53 @@ describe('Router', () => {
   it('should render 404 if no routes match', () => {
     render(<Router routes={[]} defaultComponent={() => <h1>404</h1>} />)
     expect(screen.getByText('404')).toBeTruthy()
+  })
+
+  it('should render the component of the first route that matches', () => {
+    getCurrentPath.mockReturnValue('/about')
+
+    const routes = [
+      {
+        path: '/',
+        Component: () => <h1>Home</h1>,
+      },
+      {
+        path: '/about',
+        Component: () => <h1>About</h1>,
+      },
+    ]
+
+    render(<Router routes={routes} />)
+    expect(screen.getByText('About')).toBeTruthy()
+  })
+
+  it('should navigate using Links', async () => {
+    getCurrentPath.mockReturnValueOnce('/')
+
+    render(
+      <Router>
+        <Route
+          path='/'
+          Component={() => {
+            return (
+              <>
+                <h1>Home</h1>
+                <Link to='/about'>Go to About</Link>
+              </>
+            )
+          }}
+        />
+        <Route path='/about' Component={() => <h1>About</h1>} />
+      </Router>
+    )
+
+    // Click on the link
+    const anchor = screen.getByText(/Go to About/)
+    fireEvent.click(anchor)
+
+    const aboutTitle = await screen.findByText('About')
+
+    // Check that the new route is rendered
+    expect(aboutTitle).toBeTruthy()
   })
 })
