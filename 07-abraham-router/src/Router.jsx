81ad00993046d71527e712095872475148346089
Abraham Galue <abrahamgalue512@gmail.com>
2023-10-20 13:32:32 -0400

    refactor: current path to const

--- 07-abraham-router/src/Router.jsx
+++ 07-abraham-router/src/Router.jsx
@@ -1,17 +1,18 @@
 import { EVENTS } from './consts'
 import { useEffect, useState, Children } from 'react'
 import { match } from 'path-to-regexp'
+import { getCurrentPath } from './utils/getCurrentPath'
 
 export function Router({
   children,
   routes = [],
   defaultComponent: DefaultComponent = () => <h1>404</h1>,
 }) {
-  const [currentPath, setCurrentPath] = useState(window.location.pathname)
+  const [currentPath, setCurrentPath] = useState(getCurrentPath())
 
   useEffect(() => {
     const onLocationChange = () => {
-      setCurrentPath(window.location.pathname)
+      setCurrentPath(getCurrentPath())
     }
 
     window.addEventListener(EVENTS.PUSHSTATE, onLocationChange)
