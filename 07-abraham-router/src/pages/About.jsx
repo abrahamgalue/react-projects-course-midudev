f2a76365139d544b5e919ea4cf4dbf164319154f
Abraham Galue <abrahamgalue512@gmail.com>
2023-10-21 12:53:59 -0400

    feat: change the language

--- 07-abraham-router/src/pages/About.jsx
+++ 07-abraham-router/src/pages/About.jsx
@@ -5,12 +5,14 @@ console.log('Estamos importando el archivo About.jsx')
 const i18n = {
   es: {
     title: 'Sobre nosotros',
+    changeTitle: 'Cambiar Idioma',
     description:
       'Hola! Me llamo Abraham Galue y estoy creando un clon de React Router.',
     button: 'Ir a Home Page',
   },
   en: {
     title: 'About us',
+    changeTitle: 'Change language',
     description:
       'Hello! My name is Abraham Galue and I am creating a React Router clone.',
     button: 'Go to Home Page',
@@ -26,6 +28,16 @@ export default function AboutPage({ routeParams }) {
 
   return (
     <>
+      <header>
+        <h4>{i18n.changeTitle}</h4>
+        <Link to='/es/about'>
+          {routeParams.lang === 'es' ? 'Español' : 'Spanish'}
+        </Link>
+        <br />
+        <Link to='/en/about'>
+          {routeParams.lang === 'en' ? 'English' : 'Inglés'}
+        </Link>
+      </header>
       <h1>{i18n.title}</h1>
       <div>
         <img
