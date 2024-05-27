585082ddccc7369c002441350281784d42646b38
Abraham Galue <abrahamgalue512@gmail.com>
2024-05-27 13:31:54 -0400

    React Bootstrap for styles installed

--- 09-google-translate-clone/.eslintrc.cjs
+++ 09-google-translate-clone/.eslintrc.cjs
@@ -1,18 +1,20 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  "env": {
+    "browser": true,
+    "es2021": true
   },
+  "extends": [
+    "standard-with-typescript",
+    "plugin:react/recommended"
+  ],
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "@typescript-eslint/explicit-function-return-type": 'off',
+  }
 }
