849c405fef783c31ebd88d6b0405d6cb93e20840
Abraham Galue <abrahamgalue512@gmail.com>
2024-06-14 13:04:30 -0400

    Data displayed in a table format

---
+++ 11-typescript-prueba-tecnica/src/components/UserList.tsx
@@ -0,0 +1,41 @@
+import { type User } from '../types.d'
+
+interface Props {
+  users: User[]
+}
+
+export default function UserList({ users }: Props) {
+  return (
+    <table width='100%'>
+      <thead>
+        <tr>
+          <th>Foto</th>
+          <th>Nombre</th>
+          <th>Apellido</th>
+          <th>País</th>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map(user => {
+          return (
+            <tr key={user.login.uuid}>
+              <td>
+                <img
+                  src={user.picture.thumbnail}
+                  alt={`Photo of ${user.name.first} ${user.name.last}`}
+                />
+              </td>
+              <td>{user.name.first}</td>
+              <td>{user.name.last}</td>
+              <td>{user.location.country}</td>
+              <td>
+                <button>Borrar</button>
+              </td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
+}
