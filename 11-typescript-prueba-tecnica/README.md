8fc5211a606d9a7637704db04fc37453415e998e
Abraham Galue <abrahamgalue512@gmail.com>
2024-06-14 13:04:05 -0400

    100 API users fetched

--- 11-typescript-prueba-tecnica/README.md
+++ 11-typescript-prueba-tecnica/README.md
@@ -6,10 +6,10 @@ El objetivo de esta prueba técnica es crear una aplicación similar a la que se
 
 Los pasos a seguir:
 
-- [] Fetch 100 rows of data using the API.
-- [] Display the data in a table format, similar to the example.
+- [x] Fetch 100 rows of data using the API.
+- [x] Display the data in a table format, similar to the example.
 - [] Provide the option to color rows as shown in the example.
-- [] Allow the data to be sorted by country as demonstrated in the example.
+  - [] Allow the data to be sorted by country as demonstrated in the example.
 - [] Enable the ability to delete a row as shown in the example.
 - [] Implement a feature that allows the user to restore the initial state, meaning that all deleted rows will be recovered.
 - [] Handle any potential errors that may occur.
--- 11-typescript-prueba-tecnica/src/App.tsx
+++ 11-typescript-prueba-tecnica/src/App.tsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState, useEffect } from 'react'
+import { type User } from './types'
+import UserList from './components/UserList'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [results, setResults] = useState<User[]>([])
+
+  useEffect(() => {
+    fetch('https://randomuser.me/api/?results=100')
+      .then(async res => await res.json())
+      .then(data => {
+        setResults(data.results)
+      })
+      .catch(err => {
+        console.error('Error:', err)
+      })
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Prueba técnica</h1>
+      <header>
+        <button>Colorear filas</button>
+      </header>
+      <main>
+        <UserList users={results} />
+      </main>
     </>
   )
 }
