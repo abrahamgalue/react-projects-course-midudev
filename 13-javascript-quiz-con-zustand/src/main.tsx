308deb140fb2455a59aa3c249142bf2e45d311fe
Abraham Galue <abrahamgalue512@gmail.com>
2024-06-27 13:30:25 -0400

    Dark Material UI theme added

--- 13-javascript-quiz-con-zustand/src/main.tsx
+++ 13-javascript-quiz-con-zustand/src/main.tsx
@@ -1,3 +1,5 @@
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
@@ -7,8 +9,17 @@ import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+})
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
