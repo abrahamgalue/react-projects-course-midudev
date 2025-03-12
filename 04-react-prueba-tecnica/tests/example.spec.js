50bb64f48e396553ef86c2cd5d0be9a72104b5db
Abraham Galue <abrahamgalue512@gmail.com>
2025-03-12 13:15:56 -0400

    Change the localhost URL to the production page

--- 04-react-prueba-tecnica/tests/example.spec.js
+++ 04-react-prueba-tecnica/tests/example.spec.js
@@ -2,7 +2,7 @@
 import { test, expect } from '@playwright/test'
 
 const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
-const LOCALHOST_URL = 'http://localhost:5173/'
+const LOCALHOST_URL = 'https://prueba-tecnica-abraham.netlify.app/'
 
 test('app shows random fact and image', async ({ page }) => {
   await page.goto(LOCALHOST_URL)
