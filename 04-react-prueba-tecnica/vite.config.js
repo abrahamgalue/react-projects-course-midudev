b46d2a1f26e425f60faadc1303a42fa3011548c7
Abraham Galue <abrahamgalue512@gmail.com>
2025-04-21 09:28:25 -0400

    Docker implementation changed to production only

--- 04-react-prueba-tecnica/vite.config.js
+++ 04-react-prueba-tecnica/vite.config.js
@@ -2,12 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    watch: {
-      usePolling: true
-    }
-  }
+  plugins: [react()]
 })
